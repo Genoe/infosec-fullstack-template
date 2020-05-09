8fa1ab858d3e3d8b652104ba8580d92de6b61a04
= <=>
2020-05-09 12:05:59 -0500

    Broke the searchbar out into its own component

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,11 +1,48 @@
 //Write your javascript here, or roll your own. It's up to you.
 //Make your ajax call to http://localhost:8765/api/index.php here
+function SearchBar({search, handleChange, handleSubmit}) {   
+    return (
+        <div>
+            <h2 className="justify-content-md-center text-center">Countries Search</h2>
+            <div className="text-center" id="form-container">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-row">
+                    <div className="col-md-8 form-group">
+                        <label htmlFor="search">Search</label>
+                        <input
+                            id="search"
+                            name="search" 
+                            type="text"
+                            className="form-control"
+                            value={search}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="col-md-4 form-group">
+                        <label htmlFor="searchType">Search Type</label>
+                        <select className="form-control" name="searchType" id="searchType" onChange={handleChange}>
+                            <option>Name</option>
+                            <option>Full Name</option>
+                            <option>Country Code</option>
+                        </select>
+                    </div>
+                    </div>                  
+                    <button type="submit" className="btn btn-primary">
+                        Search!
+                    </button>
+                </form>  
+            </div>   
+        </div>             
+    );  
+}
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             search: '',
             searchType: 'Name',
+            results: {}
         };
     }
     
@@ -33,37 +70,11 @@ class App extends React.Component {
   
     render() {  
         return (
-            <div>
-                <h2 className="justify-content-md-center text-center">Countries Search</h2>
-                <div className="text-center" id="form-container">
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="form-row">
-                        <div className="col-md-8 form-group">
-                            <label htmlFor="search">Search</label>
-                            <input
-                                id="search"
-                                name="search" 
-                                type="text"
-                                className="form-control"
-                                value={this.state.search}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <div className="col-md-4 form-group">
-                            <label htmlFor="searchType">Search Type</label>
-                            <select className="form-control" name="searchType" id="searchType" onChange={this.handleChange}>
-                                <option>Name</option>
-                                <option>Full Name</option>
-                                <option>Country Code</option>
-                            </select>
-                        </div>
-                        </div>                  
-                        <button type="submit" className="btn btn-primary">
-                            Search!
-                        </button>
-                    </form>  
-                </div>   
-            </div>             
+            <SearchBar
+                search={this.state.search}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+            />
        );
     }
 }
