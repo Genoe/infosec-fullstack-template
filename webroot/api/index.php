a8a8fae3b888cd3242258a78328a9b5d771da037
= <=>
2020-05-10 14:51:11 -0500

    Took out repeat code, if statement inside try/catch

--- webroot/api/index.php
+++ webroot/api/index.php
@@ -1,39 +1,33 @@
 <?php
 require_once('countriesApi.php');
 $data;
-$fullName = false;
 $url = $_SERVER['REQUEST_URI'];
+
 $pathInfo = parse_url($url, PHP_URL_PATH);
 $pathInfo = explode('/', $pathInfo);
 
-
 $api = new CountriesAPI();
 
-if (isset($_GET['fullName']) && strtolower($_GET['fullName']) !== 'false') {
-   $fullName = true;
-}
+try {
+   if ($pathInfo[3] === 'name') {
+      $fullName = false;
+      $countryName = $pathInfo[4];
 
-if ($pathInfo[3] === 'name') {
-   $countryName = $pathInfo[4];
-   try {
+      if (isset($_GET['fullName']) && strtolower($_GET['fullName']) !== 'false') {
+         $fullName = true;
+      }
       $data = $api->getCountriesByName($countryName, $fullName);
-      $data = $api->generateData($data);
-      $data['countries'] = $api->sortByPopulation($data['countries']);
-   } catch (Exception $e) {
-      http_response_code(500);
-      $data = $e->getMessage();
-   }      
-} else if ($pathInfo[3] === 'alpha') {
-   $countryCode = $pathInfo[4];
-   try {
+   } else if ($pathInfo[3] === 'alpha') {
+      $countryCode = $pathInfo[4];
       $data = $api->getCountriesByCode($countryCode);
-      $data = $api->generateData($data);
-      $data['countries'] = $api->sortByPopulation($data['countries']);
-   } catch (Exception $e) {
-      http_response_code(500);
-      $data = $e->getMessage();
-   } 
-}
+   }
+   
+   $data = $api->generateData($data);
+   $data['countries'] = $api->sortByPopulation($data['countries']);
+} catch (Exception $e) {
+   http_response_code(500);
+   $data = $e->getMessage();
+} 
 
 header('Content-Type: application/json');
 echo json_encode($data);
